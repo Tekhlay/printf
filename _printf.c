2f8d554ac3f563e7f8f8338e539473d9a56468fc
Teklay Birhane <90598109+Tekhlay@users.noreply.github.com>
2022-05-17 00:18:26 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * _printf - is a function that selects the correct function to print.
  * @format: identifier to look for.
@@ -21,25 +21,4 @@ int _printf(const char * const format, ...)
 	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-
-Here:
-	while (format[i] != '\0')
-	{
-		j = 13;
-		while (j >= 0)
-		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
-			}
-			j--;
-		}
-		_putchar(format[i]);
-		len++;
-		i++;
-	}
-	va_end(args);
-	return (len);
 }
