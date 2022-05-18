2a60870330c8934088e049145fe0a6bb01443f2c
Teklay Birhane <90598109+Tekhlay@users.noreply.github.com>
2022-05-18 21:03:57 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,15 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <limits.h>
-#include <unistd.h>
-
-
-
-
+/*Structs*/
+/*Typedefs*/
+/*Prototypes*/
+int _putchar(char c, int *k);
+void _puts(char *str, int *k);
 int _printf(const char *format, ...);
-
+void _print_number(int n, int *k);
+void _rev_string(char *s, int *k);
+int _strlen(char *s);
+void _print_binary(int n, int *k);
+void _rot13(char *s, int *k);
 #endif
