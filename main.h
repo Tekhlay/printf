d81fd18f7a1b25912631e01ccdae84c790989498
danigh1234 <gherdaniel7@gmail.com>
2022-06-13 05:37:13 -0700

    pf

--- main.h
+++ main.h
@@ -1,12 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdlib.h>
 #include <stdarg.h>
 /**
  *kprint - struct for printing diferent formats
  *@a: struct mem reperecenting date types
  *@f: function used for printing the format
  */
-typedef struct kprint
+typedef struct op
 {
 char *a;
 int (*f)(va_list);
@@ -16,14 +18,6 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 int print_c(va_list c);
 int print_s(va_list s);
-int print_i(va_list i);
-int print_d(va_list d);
-int print_u(va_list u);
-int print_b(va_list b);
-int print_o(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_S(va_list S);
-int print_r(va_list r);
-int print_R(va_list R);
+int (*modulator(const char *format))(va_list);
+int printf(const char *format, ...);
 #endif
